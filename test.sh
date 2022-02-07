7ebcaf1053a5218c37dfa01b1a68ba478f915946
monman53 <monman.cs@gmail.com>
2022-02-08 03:48:12 +0900

    エラー箇所を表示

--- test.sh
+++ test.sh
@@ -17,14 +17,17 @@ assert() {
   fi
 }
 
+# 値を1つ返す
 assert 0 0
 assert 42 42
 
+# 加減算
 assert 21 "5+20-4"
 assert 6 "30-20-4"
 assert 0 "0"
 assert 12 "1+1+2+3+5"
 
+# 空白をスキップ
 assert 41 " 12 + 34 - 5 "
 assert 12 "     12   "
 assert 5 " 4    + 1 "
